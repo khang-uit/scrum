3ff12e618a413f1f9f47120cfef6cd21a193e149
aiai142 <diuai@aiai.gmail>
2022-11-10 15:36:03 +0700

    push details

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Details from "./pages/Details/Details";
-
+import Sort from "./pages/Details/Sort/Sort";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/details/:id" element={<Details/>} />
+        <Route path="/details" element={<Details/>} />
+        <Route path="/sort" element={<Sort/>} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Details/Details.js
+++ client/src/pages/Details/Details.js
@@ -1,10 +1,61 @@
 import React from "react";
-
+import './Details.css';
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 const Details = () => {
   return (
-    <>
+    <div>
+    <nav id="topbar">
+      <span style= {{
+marginRight: 0.25+"rem"
+      }}
+      >
+        <a
+          href="https://www.comp.nus.edu.sg/~stevenha"
+          // style="text-decoration: none; color: white"
+        >
+        </a>
+        <a
+          href="https://visualgo.net"
+          style= {{
+            fontSize: " 20px",
+            marginLeft: "4px"
+          }}
 
-    </>
+        >
+          <img className="logo" src="/Home/logo.png" alt="logo" />
+        </a>
+      </span>
+      <div className="tab">
+        <button className="tablinks" >Insert</button>
+        <button className="tablinks"  >Delete</button>
+        <button className="tablinks"  >Search</button>
+      </div>
+      <br/>
+      <div className="content" style={{width:100+"%"}}>
+        <div id="Insert" className="tabcontent">
+          </div>
+          
+          <div id="Delete" className="tabcontent">
+          </div>
+          
+          <div id="Search" className="tabcontent">
+          </div>
+      </div>
+      
+      </nav>
+    </div>
   );
 };
 export default Details;
