de0d0b2d4355cf62a5e61e4c110044127f7622c3
Lov <quangdaist01@gmail.com>
2022-11-11 14:38:11 +0700

    add interactive code field

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Admin from "./pages/Admin/Admin"
 import Details from "./pages/Details/Details";
+import Sort from "./pages/Details/Sort/Sort";
+import Landing from "./react-code-editor/src/components/Landing";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/details/:id" element={<Details/>} />
+        <Route path="/sort" element={<Sort/>} />
         <Route path="/admin" element={<Admin/>} />
       </Routes>
     </BrowserRouter>
